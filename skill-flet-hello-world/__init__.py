05489726f69606eb4380a3a07b9653ec599f5221
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-12-03 14:34:44 +0100

    Simplify hello world demo skill

--- skill-flet-hello-world/__init__.py
+++ skill-flet-hello-world/__init__.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
-import os
-from typing import Optional
-from ovos_bus_client import MessageBusClient
-from ovos_bus_client.apis.gui import GUIInterface
+# import os
+# from typing import Optional
+# from ovos_bus_client import MessageBusClient
+# from ovos_bus_client.apis.gui import GUIInterface
 from ovos_workshop.skills import OVOSSkill
 from ovos_workshop.decorators import intent_handler
 
@@ -10,26 +10,8 @@ class HelloWorldSkill(OVOSSkill):
     def __init__(
         self: HelloWorldSkill,
         skill_id: str = "skill-flet-hello-world",
-        bus: Optional[MessageBusClient] = None,
     ):
-        super().__init__(
-            gui=GUIInterface(
-                skill_id=skill_id,
-                bus=bus,
-                # TODO: this should be detected automatically.
-                # There should not be the need for instatiating
-                # a GUIInterface here.
-                ui_directories={
-                    "py-flet": os.path.join(
-                        os.path.dirname(__file__),
-                        "gui",
-                        "py-flet"
-                    )
-                },
-            ),
-            skill_id=skill_id,
-            bus=bus,
-        )
+        super().__init__(skill_id=skill_id)
 
     @intent_handler("flet.intent")
     def handle_hello_world(self: HelloWorldSkill):
@@ -40,3 +22,4 @@ class HelloWorldSkill(OVOSSkill):
         self.gui.show_page("hello_world")  # Dit genereert een WebSocket-bericht
 
         self.speak(text)
+
